da38006f6c0dc6cb1cc97e8d05a3e60fac490818
Ajit Sanghera <ajit@esoftech.com>
2025-03-19 21:42:42 +0530

    fixed the issue given my seo team

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent {
-
+ 
+    contactForm: FormGroup;
+    submitted = false;
+  
+    constructor(private fb: FormBuilder) {
+      this.contactForm = this.fb.group({
+        name: ['', [Validators.required, Validators.minLength(3)]],
+        email: ['', [Validators.required, Validators.email]],
+        phone: ['', [Validators.required, Validators.pattern(/^[0-9]{10}$/)]],
+        message: ['', [Validators.required, Validators.minLength(10)]]
+      });
+    }
+  
+    onSubmit() {
+      if (this.contactForm.valid) {
+        console.log('Form Data:', this.contactForm.value);
+        this.submitted = true;
+        this.contactForm.reset();
+        setTimeout(() => this.submitted = false, 5000); // Hide success message after 5 sec
+      }
+    }
 }
