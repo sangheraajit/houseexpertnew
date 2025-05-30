49ed3178f683856acc9952a1e95b7800982088d0
Ajit Sanghera <ajit@esoftech.com>
2025-05-30 13:48:47 +0530

    local changes

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,7 +3,7 @@ export const environment = {
   production: false,
   projName: "House Expert",
   AdminServer: "https://dev.admin.houseexpert.in",
-  CommonApiServer: "https://localhost:5001/",
+  CommonApiServer: "https://dev.api.houseexpert.in/",
   ImageserverUrl: "https://dev.api.houseexpert.in/assets/UploadFile/",
   RAZORPAY_KEY_ID:'rzp_test_5kUNZLAhSBYPNy',
   RAZORPAY_KEY_SECRET:'V0sZT8L6KKgNLJZEpnvqnepb',
